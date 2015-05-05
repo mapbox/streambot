61940fcd07c9831ceeef03b96918ea60f99bfe65
Ryan Clark <ryan.clark.j@gmail.com>
2015-05-05 12:04:26 -0700

    adjust test to reflect log changes

--- test/index.test.js
+++ test/index.test.js
@@ -66,9 +66,8 @@ test('[runtime] service success', function(assert) {
 
         data.Body.toString().split('\n').forEach(function(line, i) {
           var re;
-          if (i === 0) re = /\[.+?\] \[info\] \[streambot-test\] \{"Records":\[\{"kinesis":\{"partitionKey":"partitionKey-3","kinesisSchemaVersion":"1.0","data":"SGVsbG8sIHRoaXMgaXMgYSB0ZXN0IDEyMy4=","sequenceNumber":"49545115243490985018280067714973144582180062593244200961"\},"eventSource":"aws:kinesis","eventID":"shardId-000000000000:49545115243490985018280067714973144582180062593244200961","invokeIdentityArn":"arn:aws:iam::059493405231:role\/testLEBRole","eventVersion":"1.0","eventName":"aws:kinesis:record","eventSourceARN":"arn:aws:kinesis:us-east-1:35667example:stream\/examplestream","awsRegion":"us-east-1"\}\]\}/;
-          else if (i === 1) re = /\[.+?\] \[info\] \[streambot-test\] successful/;
-          else if (i === 2) re = /\[streambot-test\] putMetricData \{"Namespace":"streambot","MetricData":\[\{"MetricName":"streambot-test-metric","Value":1,"Dimensions":\[\{"Name":"Status","Value":"Success"\}\]\}\]\}/;
+          if (i === 0) re = /\[.+?\] \[info\] \[streambot-test\] successful/;
+          else if (i === 1) re = /\[streambot-test\] putMetricData \{"Namespace":"streambot","MetricData":\[\{"MetricName":"streambot-test-metric","Value":1,"Dimensions":\[\{"Name":"Status","Value":"Success"\}\]\}\]\}/;
           else if (line) return assert.fail('extra logs sent to S3');
 
           assert.ok(re.test(line), 'expected log sent to S3');
