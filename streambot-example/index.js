ef47602d9b08b28e02d58c26d8c59b21d4366dce
Ryan Clark <ryan.clark.j@gmail.com>
2015-06-13 16:00:37 -0700

    tweak example

--- streambot-example/index.js
+++ streambot-example/index.js
@@ -3,23 +3,23 @@ var streambot = require('streambot');
 
 module.exports.streambot = streambot(exampleService);
 
-function exampleService(records, callback) {
+function exampleService(event, callback) {
   var s3 = new AWS.S3();
 
-  streambot.log.info(process.env);
+  console.log(process.env);
 
-  var record = records.shift();
+  var record = event.Records.shift();
   if (record) uploadRecord(record);
 
   function uploadRecord(record) {
     s3.putObject({
-      Bucket: 'mapbox',
-      Key: 'example-records/' + record.sequenceNumber,
-      Body: record.data
+      Bucket: process.env.EventBucket,
+      Key: process.env.EventPrefix + '/' + record.sequenceNumber,
+      Body: new Buffer(record.kinesis.data, 'base64')
     }, function(err) {
       if (err) return callback(err);
 
-      var record = records.shift();
+      var record = event.Records.shift();
       if (record) uploadRecord(record);
       else callback();
     });
