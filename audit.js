6ef1fa10d17caa7f6b30b3743fe51046f71c8b95
Ryan Clark <ryan.clark.j@gmail.com>
2017-04-26 18:47:35 -0700

    audit script

---
+++ audit.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const audit = require('@mapbox/cfn-template-audit');
+
+const templateFilter = template => /streambot/.test(template);
+audit
+  .getWorldWideTemplates({ templateFilter })
+  .then(data =>
+    process.stdout.write(
+      data
+        .map(stack => `${stack.Summary.StackName} ${stack.Region}`)
+        .join('\n') + '\n'
+    )
+  )
+  .catch(err => process.stderr.write(`${err.stack}\n`));
